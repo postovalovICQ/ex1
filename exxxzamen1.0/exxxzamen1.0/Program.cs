d3c00cd3cbc4f6e40c5d6a78cda314a0350f8ab4
postovalovICQ <71002789+postovalovICQ@users.noreply.github.com>
2020-09-23 23:54:21 +0500

    Add files via upload

---
+++ exxxzamen1.0/exxxzamen1.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace exxxzamen1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text;
+            using (StreamReader sr = new StreamReader("1.txt")) //bin\Debug\netcoreapp3.1\1.txt
+            {
+                text = sr.ReadToEnd().ToCharArray();
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ',')
+                    text[i] = '.';
+            }
+            using (StreamWriter sw = new StreamWriter("2.txt"))
+            {
+                foreach (char copy in text)
+                    sw.Write(copy);
+            }
+        }
+    }
+}
